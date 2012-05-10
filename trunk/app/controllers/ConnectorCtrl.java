4dc2177d090e4b3d1e693f5139d16aec9fb56eae
clementval@gmail.com <clementval@gmail.com@efc70731-f071-21d3-5131-b028c4b324ef>
2012-05-10 17:12:55 +0000

    - Add code to call RESTful EJB

--- trunk/app/controllers/ConnectorCtrl.java
+++ trunk/app/controllers/ConnectorCtrl.java
@@ -12,6 +12,10 @@ import play.mvc.Result;
 import views.html.*;
 import play.*;
 import play.mvc.*;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.core.util.*;
 
 
 public class ConnectorCtrl extends Controller {
@@ -19,7 +23,33 @@ public class ConnectorCtrl extends Controller {
 	private static final String pageTitle = "Connector EJB Demo";
 
 	public static Result callWithSOAP() {
-		return ok();
+		String output = "NULL";
+		try {
+ 
+			Client client = Client.create();
+	 
+			WebResource webResource = client
+			   .resource("http://localhost:8080/RESTfulEJB/manager/myresource/insert/test/ici");
+	 
+			ClientResponse response = webResource.accept("text/plain")
+	                   .get(ClientResponse.class);
+	 
+			if (response.getStatus() != 200) {
+			   throw new RuntimeException("Failed : HTTP error code : "
+				+ response.getStatus());
+			}
+	 
+			output = response.getEntity(String.class);
+	 
+			System.out.println("Output from Server .... \n");
+			System.out.println(output);
+ 
+	  } catch (Exception e) {
+ 
+			e.printStackTrace();
+ 
+	  }
+	  return ok(index.render(output));
 	}
 	
 	public static Result callWithREST() {

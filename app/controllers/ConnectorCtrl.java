3a2fd0c49885c8c7a22793af6494b33c7d88cc98
clementval <clementval@gmail.com>
2012-05-15 13:44:36 +0000

    - Add new method for form and insert from form

--- app/controllers/ConnectorCtrl.java
+++ app/controllers/ConnectorCtrl.java
@@ -21,10 +21,58 @@ import com.sun.jersey.core.util.*;
 public class ConnectorCtrl extends Controller {
 
 	private static final String pageTitle = "Connector EJB Demo";
-
+	private static Form<Article> articleForm = form(Article.class);
+	
+	
 	public static Result callWithSOAP() {
 		return ok();		
 	}
+	
+	
+	public static Result ejbform(){
+		return ok(ejbconnectorform.render("REST", articleForm));
+	}
+	
+	
+	public static Result callWithRESTForm() {
+		Form<Article> form = form(Article.class).bindFromRequest();
+		if (form.hasErrors()) {
+			return badRequest(ejbconnectorform.render("REST", form));
+		}
+
+		String topic = form.get().topic;
+		String content = form.get().content;
+		
+		String output = "NULL";
+		try {
+ 
+			Client client = Client.create();
+	 		String urlstr = "http://localhost:8080/RESTfulEJB/manager/myresource/insert/"+topic+"/"+content;
+	 
+			WebResource webResource = client
+			   .resource("http://localhost:8080/RESTfulEJB/manager/myresource/insert/test/ici");
+	 
+			ClientResponse response = webResource.accept("text/plain")
+	                   .get(ClientResponse.class);
+	 
+			if (response.getStatus() != 200) {
+			   throw new RuntimeException("Failed : HTTP error code : "
+				+ response.getStatus());
+			}
+	 
+			output = response.getEntity(String.class);
+	 
+			System.out.println("Output from Server .... \n");
+			System.out.println(output);
+ 
+	  } catch (Exception e) {
+ 
+			e.printStackTrace();
+ 
+	  }
+	  return ok(ejbconnector.render(output));
+	}
+	
 	
 	public static Result callWithREST() {
 		String output = "NULL";
@@ -53,10 +101,10 @@ public class ConnectorCtrl extends Controller {
 			e.printStackTrace();
  
 	  }
-	  return ok(index.render(output));
+	  return ok(ejbconnector.render(output));
 	}
 	
-	public static Result callWithRMI() {
+	public static Result callWithSpringRemote() {
 		return ok();
 	}
 }

c790e68432517515648691422cc391d6ca5e8168
clementval@gmail.com <clementval@gmail.com@efc70731-f071-21d3-5131-b028c4b324ef>
2012-05-16 18:53:48 +0000

    - Push netbeans project Spring remoting client side implementation

---
+++ trunk/ejb/SpringRClient/src/main/java/com/eif/osf/springremoting/service/BlogManagerInt.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.eif.osf.springremoting.service;
+
+/**
+ *
+ * @author clementval
+ */
+
+public interface BlogManagerInt {
+    public String getMessage();
+
+    public void setMessage(String mesg);
+}

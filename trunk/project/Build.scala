d8041ac551a78e469e17c170ebc85d9dcde77450
clementval@gmail.com <clementval@gmail.com@efc70731-f071-21d3-5131-b028c4b324ef>
2012-05-16 18:50:33 +0000

    - Add dependencies for Spring and Spring Remoting

--- trunk/project/Build.scala
+++ trunk/project/Build.scala
@@ -9,8 +9,12 @@ object ApplicationBuild extends Build {
 
     val appDependencies = Seq(
 		"com.sun.jersey" % "jersey-client" % "1.8",
-		"com.sun.jersey" % "jersey-core" % "1.8"
-    )
+		"com.sun.jersey" % "jersey-core" % "1.8",
+		"org.springframework" % "spring-core" % "2.5.4",
+		"org.springframework" % "spring-context" % "2.5.4",
+		"org.springframework" % "spring-aop" % "2.5.4",
+		"org.springframework" % "spring-remoting" % "2.0.8"				
+	)
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = JAVA).settings(
       // Add your own project settings here      
